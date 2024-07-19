933ab2ededec6ff3cb04a1a16a7cff5a0344c571
DAJEONGHADA <112385274+DAJEONGHADA@users.noreply.github.com>
2024-07-19 09:35:45 +0900

    Time: 37 ms (53.36%), Space: 16.4 MB (68.84%) - LeetHub

--- 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.py
+++ 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.py
@@ -1,20 +1,31 @@
 class Solution:
     def numDifferentIntegers(self, word: str) -> int:
-        # tmp = ""
-        # for c in word:
-        #     if c.isnumeric():
-        #         tmp += c
-        #     elif tmp:
-        #         print(tmp)
-        #         tmp = ""
-        #     if tmp:
-        #         print(tmp)
-        #     return 0
+        nums = set()
+        tmp = ""
+        for c in word:
+            if c.isnumeric():
+                tmp += c
+            elif tmp:
+                nums.add(int(tmp))
+                tmp = ""
+        if tmp:
+            nums.add(int(tmp))
+        return len(nums)
         
-        numbers = re.findall(r'\d+', word)
-        # 숫자 앞의 0을 제거하고 정수로 변환하여 집합에 추가
-        unique_numbers = {int(number) for number in numbers}
-        return len(unique_numbers)
         
+        
+        
+        
+        
+        
+        
+        
+        
+        # # 정규 표현식을 사용하여 숫자 문자열 추출
+        # numbers = re.findall(r'\d+', word)
+        # # 숫자 문자열을 정수로 변환하고 집합에 저장하여 중복 제거
+        # unique_numbers = {int(number) for number in numbers}
+        # # 고유한 숫자의 개수를 반환
+        # return len(unique_numbers)
         
         
