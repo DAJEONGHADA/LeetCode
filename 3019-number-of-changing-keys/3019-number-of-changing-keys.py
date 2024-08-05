71d464ca79cf41667e5edf4af8f5eb4b3a016887
DAJEONGHADA <112385274+DAJEONGHADA@users.noreply.github.com>
2024-08-05 09:38:14 +0900

    Time: 35 ms (70.05%), Space: 16.4 MB (62.23%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.py
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.py
@@ -1,7 +1,18 @@
+# class Solution:
+#     def countKeyChanges(self, s: str) -> int:
+#         answer = 0
+#         for i in range(1, len(s)):
+#             # s의 i번째 문자의 소문자가 i-1번째 문자의 소문자와 같지 않은지
+#             if s[i].lower() != s[i - 1].lower():
+#                 answer += 1
+#         return answer
+
 class Solution:
     def countKeyChanges(self, s: str) -> int:
         answer = 0
-        for i in range(1, len(s)):
-            if s[i].lower() != s[i - 1].lower():
+        # 문자열 lower, for문 사용해서 앞에 문자와 뒤에 문자 비교
+        s = s.lower()
+        for i in range(len(s) - 1):
+            if s[i].lower() != s[i+1].lower():
                 answer += 1
         return answer
