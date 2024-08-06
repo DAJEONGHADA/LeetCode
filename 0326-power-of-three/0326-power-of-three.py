4bfb570344d35561ab40ab6abbb8f1345209342e
DAJEONGHADA <112385274+DAJEONGHADA@users.noreply.github.com>
2024-08-06 09:22:26 +0900

    Time: 67 ms (53.39%), Space: 16.6 MB (45.55%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        # while문 사용
+        if n <= 0:
+            return False
+        while n % 3 == 0:
+            n //= 3        # n //= 3는 n을 3으로 나눈 몫으로 갱신, n이 3의 거듭제곱인지 확인하기
+        return n == 1      # 1이라면 n은 3의 거듭제곱이므로 True를 반환하고, 그렇지 않으면 False를 반환
