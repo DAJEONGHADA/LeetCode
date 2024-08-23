da768c0983717fe17a5d0bd141d88d9f87281993
DAJEONGHADA <112385274+DAJEONGHADA@users.noreply.github.com>
2024-08-23 09:39:46 +0900

    Time: 35 ms (66.25%), Space: 16.5 MB (70.83%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,10 +1,11 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        while num >= 10:  # num이 한 자릿수가 될 때까지 반복
-            sum = 0
-            while num > 0:
-                sum += num % 10  # num의 마지막 자릿수를 더함
-                num //= 10  # 마지막 자릿수를 제거
-            num = sum  # sum을 num으로 갱신
-
+        # while문, 숫자 -> 문자열 -> 숫자
+        while num >= 10:
+            # 각 자릿 수의 합계를 num에다가 입력
+            digit_sum = 0
+            for c in str(num):
+                digit_sum += int(c)
+            num = digit_sum
         return num
+    
